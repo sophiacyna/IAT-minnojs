1d5eaadc7421ebdc7315f4c66d5ecc3efe25d6b4
sophiacyna <sophia.cyna@gmail.com>
2023-07-18 13:39:21 +0930

    changed link to exact-copy

--- set-stimuli.js
+++ set-stimuli.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://sophiacyna.github.io/IAT-minnojs/exact-copy.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
